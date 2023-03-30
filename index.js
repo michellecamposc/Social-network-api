cbced42e3c23fd27591637c167c838c0dc62bfe6
michellecamposc <stephaniecampoos95@gmail.com>
2023-03-30 13:26:20 -0400

    Added run-project.sh script to automate project setup with Docker and NPM start

---
+++ index.js
@@ -0,0 +1,10 @@
+//Database connection
+
+
+
+
+// Create node server
+
+// Cors config
+
+// Convert the data of the body to js objects
---
+++ run-project.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /Users/michellecampos/Social-network-api
+docker start socialNetwork
+npm start
+
+# ./run-project.sh
