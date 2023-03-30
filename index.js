310c58466e214cc8ad801b42f9f20daf003039b2
michellecamposc <stephaniecampoos95@gmail.com>
2023-03-30 14:44:19 -0400

    Node server, convert the data of the body to json objects and Listening to http requests added

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const connection = require("./database/connection");
+const express = require("express")
+const cors = require("cors")
 
 //Database connection
 console.log("Node API for social Network initialized");
 connection();
 
-
 // Create node server
+const app = express();
+const port = 3000;
 
 // Cors config
+app.use(cors());
 
-// Convert the data of the body to js objects
+// Convert the data of the body to json objects
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Listening to http requests
+app.listen(port, () => {
+  console.log("Node server", port);
+})
