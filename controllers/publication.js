d48278c0e997e18878e1bd9d39af7b268dbe80f0
michellecamposc <stephaniecampoos95@gmail.com>
2023-04-09 15:49:21 -0400

    Publication model created

--- controllers/publication.js
+++ controllers/publication.js
@@ -1,8 +1,15 @@
+const Publication = require("../models/publication");
+
 const publicationTest = (req, res) => {
   return res.status(200).send({
     message: "Sent message from: controllers/publication.js"
   });
 }
+
+
+
+
+
 
 module.exports = {
   publicationTest
---
+++ models/publication.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const PublicationSchema = Schema({
+  user: {
+    type: Schema.ObjectId,
+    ref: "User",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  file: String,
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = model("Publicaytion", PublicationSchema, "publications");
